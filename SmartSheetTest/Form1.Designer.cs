cb0219c13dc2a270e45d18f7123f33bcb2adb7bc
gtouss <gtouss@gmail.com>
2015-05-05 17:51:33 -0400

    timisannoying

---
+++ SmartSheetTest/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SmartSheetTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListSheets = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.UpdateSheet = new System.Windows.Forms.Button();
+            this.ListColumns = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListSheets
+            // 
+            this.ListSheets.Location = new System.Drawing.Point(12, 12);
+            this.ListSheets.Name = "ListSheets";
+            this.ListSheets.Size = new System.Drawing.Size(87, 23);
+            this.ListSheets.TabIndex = 0;
+            this.ListSheets.Text = "ListSheets";
+            this.ListSheets.UseVisualStyleBackColor = true;
+            this.ListSheets.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(12, 103);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(392, 236);
+            this.Output.TabIndex = 1;
+            // 
+            // UpdateSheet
+            // 
+            this.UpdateSheet.Location = new System.Drawing.Point(12, 74);
+            this.UpdateSheet.Name = "UpdateSheet";
+            this.UpdateSheet.Size = new System.Drawing.Size(87, 23);
+            this.UpdateSheet.TabIndex = 2;
+            this.UpdateSheet.Text = "Update Sheet";
+            this.UpdateSheet.UseVisualStyleBackColor = true;
+            this.UpdateSheet.Click += new System.EventHandler(this.UpdateSheet_Click);
+            // 
+            // ListColumns
+            // 
+            this.ListColumns.Location = new System.Drawing.Point(12, 45);
+            this.ListColumns.Name = "ListColumns";
+            this.ListColumns.Size = new System.Drawing.Size(87, 23);
+            this.ListColumns.TabIndex = 3;
+            this.ListColumns.Text = "List Columns";
+            this.ListColumns.UseVisualStyleBackColor = true;
+            this.ListColumns.Click += new System.EventHandler(this.ListColumns_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(410, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(910, 350);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ListColumns);
+            this.Controls.Add(this.UpdateSheet);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.ListSheets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ListSheets;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button UpdateSheet;
+        private System.Windows.Forms.Button ListColumns;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ SmartSheetTest/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using Smartsheet.Api.OAuth;
+using System.Collections;
+
+namespace SmartSheetTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Set the Access Token
+            Token token = new Token();
+            token.AccessToken = "4ox8qvd0qyaj6y12vn302ccwy1";
+
+            // Use the Smartsheet Builder to create a Smartsheet
+            SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(token.AccessToken).Build();
+
+            // Get home
+            Home home = smartsheet.Home().GetHome(new ObjectInclusion[] { ObjectInclusion.TEMPLATES });
+
+            // List Sheets
+            IList<Sheet> homeSheets = smartsheet.Sheets().ListSheets();
+            foreach (Sheet tmpSheet in homeSheets)
+            {
+                
+            	Console.WriteLine("sheet:" + tmpSheet.ID + " " + tmpSheet.Name);
+                Output.Text = Output.Text + tmpSheet.ID + " " + tmpSheet.Name + "\r\n";
+            }
+        }
+
+        private void UpdateSheet_Click(object sender, EventArgs e)
+        {
+            // Set the Access Token
+            Token token = new Token();
+            token.AccessToken = "4ox8qvd0qyaj6y12vn302ccwy1";
+
+            // Use the Smartsheet Builder to create a Smartsheet
+            SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(token.AccessToken).Build();
+
+            connString = "Server=srv-it3.celerant.com;Database=SupportSSRS;Trusted_Connection=Yes;";
+            DataTable table1 = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                cmd = "select customer,Incident_ID,AssignedTo,cast(StartDate as date),cast(LastActionDate as date),Problem from supportssrs.dbo.TB_INCIDENT where StartDate<dateadd(dd,-7,cast(getdate() as date)) and TeamName like '%team%' and enddate is null";
+                using (SqlDataAdapter reader = new SqlDataAdapter(cmd, conn))
+                {
+                    reader.Fill(table1);
+                    dataGridView1.DataSource = table1;
+                }
+            }
+            foreach (DataRow dtrow in table1.Rows)
+            {
+                Cell cellClient = new Cell();
+                cellClient.Value = dtrow.ItemArray[0];
+                cellClient.Strict = false;
+                cellClient.ColumnId = 7656332250638212;
+                Cell cellIncidentID = new Cell();
+                cellIncidentID.Value = dtrow.ItemArray[1];
+                cellIncidentID.ColumnId = 2026832716425092;
+                Cell cellAssignedTo = new Cell();
+                cellAssignedTo.Value = dtrow.ItemArray[2];
+                cellAssignedTo.ColumnId = 1078710095898500;
+                Cell cellStartDate = new Cell();
+                cellStartDate.Value = dtrow.ItemArray[3];
+                cellStartDate.ColumnId = 5582309723268996;
+                Cell cellLastActionDate = new Cell();
+                cellLastActionDate.Value = dtrow.ItemArray[4];
+                cellLastActionDate.ColumnId = 3330509909583748;
+                Cell cellProblem = new Cell();
+                cellProblem.Value = dtrow.ItemArray[5];
+                cellProblem.ColumnId = 7834109536954244;
+
+                //// Store the cells in a list
+                List<Cell> cells1 = new List<Cell>();
+                cells1.Add(cellClient);
+                cells1.Add(cellIncidentID);
+                cells1.Add(cellAssignedTo);
+                cells1.Add(cellStartDate);
+                cells1.Add(cellLastActionDate);
+                cells1.Add(cellProblem);
+                //// Create a row and add the list of cells to the row
+                Row row = new Row();
+
+                row.Cells = cells1;
+                //// Add two rows to a list of rows.
+                List<Row> rows = new List<Row>();
+                rows.Add(row);
+                RowWrapper rowWrapper = new RowWrapper.InsertRowsBuilder().SetRows(rows).SetToBottom(true).Build();
+                smartsheet.Sheets().Rows().InsertRows(1449506131732356, rowWrapper);
+            }
+        }
+
+        private void ListColumns_Click(object sender, EventArgs e)
+        {
+            // Set the Access Token
+            Token token = new Token();
+            token.AccessToken = "4ox8qvd0qyaj6y12vn302ccwy1";
+
+            // Use the Smartsheet Builder to create a Smartsheet
+            SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(token.AccessToken).Build();
+            IList<Column> sheetCols = smartsheet.Sheets().Columns().ListColumns(1449506131732356);
+            foreach (Column tmpCols in sheetCols)
+            {
+
+                Console.WriteLine("sheet:" + tmpCols.ID + " " + tmpCols.Title);
+                Output.Text = Output.Text + tmpCols.ID + " " + tmpCols.Title + "\r\n";
+            }
+        }
+
+        public string connString { get; set; }
+
+        public string cmd { get; set; }
+
+    }
+}
